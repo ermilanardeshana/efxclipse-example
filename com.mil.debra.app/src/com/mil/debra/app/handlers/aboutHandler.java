9084d8d7529059ff076e926f5d6d84220d62f029
Milan <Rushi@LAPTOP-T8BTA4R6.PUBLIC.co.fairfax.va.us>
2016-11-22 14:42:21 -0500

    Fixes #2

--- com.mil.debra.app/src/com/mil/debra/app/handlers/aboutHandler.java
+++ com.mil.debra.app/src/com/mil/debra/app/handlers/aboutHandler.java
@@ -5,13 +5,17 @@ import org.eclipse.e4.core.di.annotations.Execute;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+/**
+ * @author Milan
+ *
+ */
 public class aboutHandler {
 	@Execute
 	public void execute() {
 	  	Alert alert = new Alert(AlertType.INFORMATION);
     	alert.setTitle("AddressApp");
-    	alert.setHeaderText("About");
     	alert.setContentText("Author: Milan Ardeshana\nWebsite: https://milanardeshana.wordpress.com/");
+    	alert.setHeaderText("About");
     	alert.showAndWait();
 	}
 
